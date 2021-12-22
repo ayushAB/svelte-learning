7c3466a438bd126aa9c1402346de15455464624b
Ayush <ayushbante6@gmail.com>
2021-12-22 21:00:09 +0530

    simple counter

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script>
-	export let name;
+	let count = 0;
+
+	function increment() {
+		count++;
+	}
+	function decrement() {
+		count--;
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Count {count}!</h1>
+	<div>
+		<button on:click={decrement}>-</button>
+		<button on:click={increment}>+</button>
+	</div>
+	
 </main>
 
 <style>
